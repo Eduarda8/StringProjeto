061b59304f8eeba39d1df1bd5366047c1511d06b
Eduarda-Alves <eduarda.alves1170@gmail.com>
2016-10-03 19:00:05 -0300

    Atividade sobre String

---
+++ Principal.py
@@ -0,0 +1,7 @@
+import StringUtil
+
+texto = input("Digite um texto :")
+StringUtil.output(texto)
+StringUtil.tamanho(texto)
+StringUtil.maiusculo(texto)
+StringUtil.minusculo(texto)
---
+++ StringUtil.py
@@ -0,0 +1,21 @@
+'''
+Atividade sobre Strings ^^ 
+'''
+
+def output (texto):
+    print(texto)
+    
+def tamanho (texto):
+    tamanho_texto = len(texto)
+    print ("tamanho_texto" ,tamanho)
+    return tamanhotexto
+
+def maiusculo (texto)
+    maiusculo = texto.upper()
+    print ("maiusculo" ,maiusculo)
+    return  maiusculo
+
+def minusculo(texto)
+    minusculo = texto.lower
+    print ("minusculo" ,minusculo)
+    return minusculo
